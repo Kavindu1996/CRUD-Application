420ece63c897658c4669f6c148d67c2152b35b2d
Kavindu 1996 <kavindugeeshan443@gmail.com>
2022-10-02 14:16:37 +0530

    back end part

---
+++ back-end/sprintboot-crud-operation/src/main/java/com/project/sprintbootcrudoperation/SprintbootCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.project.sprintbootcrudoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootCrudOperationApplication.class, args);
+	}
+
+}
---
+++ back-end/sprintboot-crud-operation/src/test/java/com/project/sprintbootcrudoperation/SprintbootCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sprintbootcrudoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
